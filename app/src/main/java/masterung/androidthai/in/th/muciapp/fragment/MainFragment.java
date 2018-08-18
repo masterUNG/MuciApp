8f74ef8ad5de572b86ab00fef3ca288235e3a65d
masterUNG <phrombutr@gmail.com>
2018-08-18 11:05:05 +0700

    Loop OK

--- app/src/main/java/masterung/androidthai/in/th/muciapp/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/muciapp/fragment/MainFragment.java
@@ -8,9 +8,11 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,11 +22,29 @@ import masterung.androidthai.in.th.muciapp.R;
 
 public class MainFragment extends Fragment{
 
+    //    Explicit
+    private Handler handler = new Handler();
+    private int timesAnInt = 0;
+    private  Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            timesAnInt += 1;
+            Log.d("18AugV1", "time ==> " + timesAnInt);
+            if (timesAnInt == 10) {
+                myNotification();
+            }
+
+            handler.postDelayed(runnable, 1000);
+
+        }
+    };
+
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        myNotification();
+        handler.postDelayed(runnable, 1000);
 
     }   // Main Method
 
