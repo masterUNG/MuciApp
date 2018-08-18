d4fbcd38cfcfe8bb32ac06d49cbd05615f241eaa
masterUNG <phrombutr@gmail.com>
2018-08-18 10:52:44 +0700

    Noti OK

--- app/src/main/java/masterung/androidthai/in/th/muciapp/NotificaionActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/muciapp/NotificaionActivity.java
@@ -3,11 +3,21 @@ package masterung.androidthai.in.th.muciapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import masterung.androidthai.in.th.muciapp.fragment.NotificationFragment;
+
 public class NotificaionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notificaion);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentFragmentNotification, new NotificationFragment())
+                    .commit();
+        }
+
     }
 }
--- app/src/main/java/masterung/androidthai/in/th/muciapp/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/muciapp/fragment/MainFragment.java
@@ -1,5 +1,12 @@
 package masterung.androidthai.in.th.muciapp.fragment;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import masterung.androidthai.in.th.muciapp.NotificaionActivity;
 import masterung.androidthai.in.th.muciapp.R;
 
 public class MainFragment extends Fragment{
@@ -16,9 +24,37 @@ public class MainFragment extends Fragment{
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-
+        myNotification();
 
     }   // Main Method
+
+    private void myNotification() {
+
+        Intent intent = new Intent(getActivity(), NotificaionActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                getActivity(),
+                (int) System.currentTimeMillis(),
+                intent,
+                0);
+
+        Uri uri = RingtoneManager.getDefaultUri(Notification.DEFAULT_SOUND);
+
+        Notification notification = new Notification.Builder(getActivity())
+                .setContentTitle("This is Title")
+                .setContentText("This is Text")
+                .setSmallIcon(R.drawable.ic_action_noti)
+                .setSound(uri)
+                .setContentIntent(pendingIntent).build();
+
+        NotificationManager notificationManager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        notificationManager.notify(0, notification);
+
+
+
+
+
+    }
 
     @Nullable
     @Override
