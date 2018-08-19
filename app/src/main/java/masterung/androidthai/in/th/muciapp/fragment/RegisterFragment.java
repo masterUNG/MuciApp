e2443aad2aea44f3a3ab9602a880d021ec817723
masterUNG <phrombutr@gmail.com>
2018-08-19 10:20:34 +0700

    Check Space Success

--- app/src/main/java/masterung/androidthai/in/th/muciapp/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/muciapp/fragment/RegisterFragment.java
@@ -7,10 +7,59 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import masterung.androidthai.in.th.muciapp.R;
+import masterung.androidthai.in.th.muciapp.utility.MyAlert;
 
-public class RegisterFragment extends Fragment{
+public class RegisterFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        registerController();
+
+    }   // Main Method
+
+    private void registerController() {
+        Button button = getActivity().findViewById(R.id.btnRegister);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText nameEditText = getView().findViewById(R.id.edtName);
+                EditText lastNameEditText = getView().findViewById(R.id.edtLastName);
+                EditText emailEditText = getView().findViewById(R.id.edtEmail);
+                EditText phoneEditText = getView().findViewById(R.id.edtPhone);
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String nameString = nameEditText.getText().toString().trim();
+                String lastNameString = lastNameEditText.getText().toString().trim();
+                String emailString = emailEditText.getText().toString().trim();
+                String phoneString = phoneEditText.getText().toString().trim();
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                MyAlert myAlert = new MyAlert(getActivity());
+
+                if (nameString.isEmpty() || lastNameString.isEmpty() ||
+                        emailString.isEmpty() || phoneString.isEmpty() ||
+                        userString.isEmpty() || passwordString.isEmpty()) {
+                        myAlert.normalDialog("Have Space",
+                                "Please Fill Every Blank");
+                } else {
+//                    Non Space
+
+                }
+
+
+            }
+        });
+    }
 
     @Nullable
     @Override
--- app/src/main/java/masterung/androidthai/in/th/muciapp/utility/MyConstant.java
+++ app/src/main/java/masterung/androidthai/in/th/muciapp/utility/MyConstant.java
@@ -4,6 +4,11 @@ public class MyConstant {
 
     //    URL
     private String urlTestString = "http://androidthai.in.th/muz/getTestMaster.php";
+    private String urlAddNewUser = "http://43.229.78.211/sigvip/androidService/addUser.php";
+
+    public String getUrlAddNewUser() {
+        return urlAddNewUser;
+    }
 
     public String getUrlTestString() {
         return urlTestString;
